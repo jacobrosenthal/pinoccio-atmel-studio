c3b731f9b786a189dcc52b0a96ea13a8ebf6ed8d
jacobrosenthal <jakerosenthal@gmail.com>
2015-05-07 21:31:53 -0700

    remove debug build folder

--- Bootstrap/Bootstrap/Debug/libs/hooks.d
+++
@@ -1,2 +0,0 @@
-libs/hooks.d libs/hooks.o: \
- ../../../../../../../../Program\ Files/Arduino/hardware/arduino/avr/cores/arduino/hooks.c
--- Bootstrap/Bootstrap/Debug/libs/js0n.d
+++
@@ -1,1 +0,0 @@
-libs/js0n.d libs/js0n.o: ../../../../../Arduino/libraries/js0n/js0n.c
